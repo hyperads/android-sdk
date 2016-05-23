7c919e4d559a5723e778b1f59b577b98f181077f
7va <7va.motw@gmail.com>
2016-05-23 13:33:55 +0300

    update to v1.2.1

--- app/src/main/java/hyperadx/sample/MainActivity.java
+++ app/src/main/java/hyperadx/sample/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(dispply.com.hadsample.R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
         showNativeAd();
 
